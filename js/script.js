7706e2d0de148aef46c1cacf1536b1e066c51f4f
Lucastec35 <captadormiranda@gmail.com>
2024-11-03 13:59:47 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Definir a data final da promoção
+const countdownDate = new Date("NOV 31, 2024 23:59:59").getTime();
+
+const countdown = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    // Calculo dias, horas, minutos e segundos
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // contagem regressiva
+    document.getElementById("days").innerText = days.toString().padStart(2, '0');
+    document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
+
+    // Quando a contagem acabar
+    if (distance < 0) {
+        clearInterval(countdown);
+        document.getElementById("promoCountdown").innerText = "Promoção Expirada";
+    }
+}, 1000);
