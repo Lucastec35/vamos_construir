4c34f113b0cb1270166d9f37471de282a5f4e207
Lucastec35 <captadormiranda@gmail.com>
2024-11-15 19:20:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Definir a data final da promoção
-const countdownDate = new Date("FEV 28, 2024 23:59:59").getTime();
+const countdownDate = new Date("FEB 28, 2024 23:59:59").getTime();
 
 const countdown = setInterval(() => {
     const now = new Date().getTime();
